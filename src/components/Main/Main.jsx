1086d43ddae59cee8bbb08e1853766db1546805e
Mariia Skydan <83235548+maskaa22@users.noreply.github.com>
2024-06-07 17:14:17 +0300

    Update Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -160,7 +160,7 @@ const Main = () => {
       //   data: formData
       //  })
 
-       await axios.post('http://localhost:5000/upload-image', formData, {
+       await axios.post('https://telegtam-bot-node.onrender.com/upload-image', formData, {
              headers: {
                  'Content-Type': 'multipart/form-data',
              },
